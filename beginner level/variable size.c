86b6b07b838f4296a14d7b7e578d0c86518aecb1
ANUMUNIRAJU <34641587+ANUMUNIRAJU@users.noreply.github.com>
2017-12-18 14:21:20 +0530

    Create variable size.c

---
+++ beginner level/variable size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+char a[10]="sachin";
+char b[10]="sach";
+int r=10;
+float avg=142.9;
+int m=10;
+printf("%s",a);
+printf("\n%s",b);
+printf("\n%d",r);
+printf("\n%d",m);
+printf("\n%f",avg);	
+return 0;
+}
